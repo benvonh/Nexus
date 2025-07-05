e8a0a5bfa90f2e926418695732578613dfb8352a
benvonh <benjaminvonsnarski@gmail.com>
2025-07-05 19:25:11 +1000

    Cleaned up CMake and can build in Debug mode now!

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,4 @@
 call C:\pixi_ws\ros2-windows\setup.bat
 cd C:\pixi_ws\DigitalTwin\build
-pixi run --manifest-path C:\pixi_ws cmake --build . --target app --config RelWithDebInfo -- /m
+pixi run --manifest-path C:\pixi_ws cmake --build . --target app -- /m
+@REM pixi run --manifest-path C:\pixi_ws cmake --build . --target app --config RelWithDebInfo -- /m
